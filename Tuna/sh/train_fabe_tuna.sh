cb09a1d5f8b1626f3746153f6316b0716bc70fb5
Shulin Li <jp2022213211@qmul.ac.uk>
2025-10-18 22:38:38 +0800

    Add A6000 Support

--- Tuna/sh/train_fabe_tuna.sh
+++ Tuna/sh/train_fabe_tuna.sh
@@ -12,7 +12,38 @@ LOG_DIR="logs/tuna-deepseek-coder-6.7b-instruct"
 mkdir -p $OUTPUT_DIR
 mkdir -p $LOG_DIR
 
-# --- 单卡 A100 80G 训练命令 ---
+## --- 单卡 A100 80G 训练命令 ---
+#python /home/nfs/share-yjy/dachuang2025/m2026-lsl/FABE/Tuna/src/train_tuna.py \
+#    --model_name_or_path $MODEL_PATH \
+#    --data_path $TRAIN_DATA_PATH \
+#    --output_dir $OUTPUT_DIR \
+#    --logging_dir $LOG_DIR \
+#    \
+#    --num_train_epochs 3 \
+#    --per_device_train_batch_size 4 \
+#    --gradient_accumulation_steps 8 \
+#    --learning_rate 2e-5 \
+#    --model_max_length 2048 \
+#    --optim "adamw_torch" \
+#    --dataloader_num_workers 0 \
+#    \
+#    --peft lora \
+#    --lora_r 16 \
+#    --lora_alpha 32 \
+#    --lora_dropout 0.05 \
+#    \
+#    --chat_template deepseek \
+#    --bf16 True \
+#    --tf32 True \
+#    --gradient_checkpointing True \
+#    \
+#    --logging_steps 10 \
+#    --save_strategy "steps" \
+#    --save_steps 200 \
+#    --save_total_limit 3 \
+#    --report_to "tensorboard"
+
+# --- 针对 A6000 调整的训练命令 ---
 python /home/nfs/share-yjy/dachuang2025/m2026-lsl/FABE/Tuna/src/train_tuna.py \
     --model_name_or_path $MODEL_PATH \
     --data_path $TRAIN_DATA_PATH \
@@ -20,8 +51,8 @@ python /home/nfs/share-yjy/dachuang2025/m2026-lsl/FABE/Tuna/src/train_tuna.py \
     --logging_dir $LOG_DIR \
     \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 16 \
     --learning_rate 2e-5 \
     --model_max_length 2048 \
     --optim "adamw_torch" \
